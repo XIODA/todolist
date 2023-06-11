d52d0f037d246c20659e2af3c15933be148b081b
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-11 20:32:36 +0800

    新增完成、刪除功能

--- backend/APIs/task/content_1.php
+++ backend/APIs/task/content_1.php
@@ -3,17 +3,17 @@ include('./configure.php');
 $link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
 session_start();
 
-$query = 'SELECT * FROM `test`.`todolist`';
+$query = 'SELECT * FROM `test`.`todolist` where finish=0';
 $smt = $link->prepare($query);
 $smt->execute();
 $reasult = $smt->fetchall(PDO::FETCH_ASSOC);
-
+$num = 1;
 foreach ($reasult as $row)
          
 {
-   
+    
     echo '<tr>';
-    echo '<td style="width: 5%">' . $row['id'] . '</td>';
+    echo '<td style="width: 5%">' . $num . '</td>';
     echo '<td style="width: 10%">' . $row['task'] . '</td>';
     echo '<td style="width: 15%">' . $row['context'] . '</td>';
     echo '<td style="width:50%">' . $row['date'] . '</td>';
@@ -21,9 +21,14 @@ foreach ($reasult as $row)
     <form action="./backend/APIs/update/update.php" method="post">
     <input type="text" value="'.$row['id'].'"name="uid"" hidden>
     <input type="submit" value="修改" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form>  
-    <form action="./backend/APIs/finish/finish.php" method="post">
-    <input type="submit" value="完成" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form></td>';
+    <form action="./finish.php" method="post">
+    <input type="text" value="'.$row['id'].'"name="uid"" hidden>
+    <input type="submit" value="完成" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form>
+    <form action="./finish_del.php" method="post">
+    <input type="text" value="'.$row['id'].'"name="uid"" hidden>
+    <input type="submit" value="刪除" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form></td>';
+    
     echo '</tr>';
-
+    $num++;
 }
 
