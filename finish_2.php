989d2c7ec3bca9a336e826a8a452d9e2f0c2c37c
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-11 20:33:59 +0800

    更改finish未完成 = 0

---
+++ finish_2.php
@@ -0,0 +1,18 @@
+<?php
+// exit;
+include('./configure.php');
+$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
+$uid = "";
+if (isset($_POST['uid'])) {
+    $uid = $_POST['uid'];
+}
+
+$finish = 0;
+
+if ($uid != "") {
+    $sql = "UPDATE `test`.`todolist` SET `finish` =  $finish  where `id` = ? ";
+    $data = [$uid];
+    $stmt = $link->prepare($sql);
+    $stmt->execute($data);
+    header("Location: ./index.php");
+}
