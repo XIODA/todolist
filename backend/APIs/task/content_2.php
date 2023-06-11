a41e0d82626218035bf8a951db70213b2ebd63dc
吳勖群 <fvgbhn321597@yahoo.com.tw>
2023-06-11 20:35:31 +0800

    如果finish = 1

---
+++ backend/APIs/task/content_2.php
@@ -0,0 +1,33 @@
+<?php
+include('./configure.php');
+$link = new PDO('mysql:host=' . $hostname . ';dbname=' . $database . ';charset=utf8', $username, $password);
+
+$query = 'SELECT * FROM `test`.`todolist` where finish=1';
+$smt = $link->prepare($query);
+$smt->execute();
+$reasult = $smt->fetchall(PDO::FETCH_ASSOC);
+$num = 1;
+foreach ($reasult as $row)
+         
+{
+    
+    echo '<tr>';
+    echo '<td style="width: 5%">' . $num . '</td>';
+    echo '<td style="width: 10%">' . $row['task'] . '</td>';
+    echo '<td style="width: 15%">' . $row['context'] . '</td>';
+    echo '<td style="width:50%">' . $row['date'] . '</td>';
+    echo '<td style="width: 0%">
+    <form action="./backend/APIs/update/update.php" method="post">
+    <input type="text" value="'.$row['id'].'"name="uid"" hidden>
+    <input type="submit" value="修改" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form>  
+    <form action="./finish_2.php" method="post">
+    <input type="text" value="'.$row['id'].'"name="uid"" hidden>
+    <input type="submit" value="未完成" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form>
+    <form action="./finish_del.php" method="post">
+    <input type="text" value="'.$row['id'].'"name="uid"" hidden>
+    <input type="submit" value="刪除" style="background-color:#685F74 ; color: white ; font-size: 17px; cursor: pointer;"></form></td>';
+    
+    echo '</tr>';
+    $num++;
+}
+
